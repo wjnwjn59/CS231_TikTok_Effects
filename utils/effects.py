0c65aff699fb9016bb1d036876ce49aa71c1b6e1
ThangDuong59 <60088921+ThangDuong59@users.noreply.github.com>
2021-11-01 01:13:19 +0700

    add comments for functions

--- utils/effects.py
+++ utils/effects.py
@@ -3,17 +3,21 @@ import pixellib
 from pixellib.tune_bg import alter_bg
 
 def do_nothing_effect(frame):
-
+    """
+    If no effect is specified, then this function will be used because it returns original frame.
+    """
     return frame
 
-def background_removal_effect(frame, background_img):
+def background_removal_effect(frame, background_img_path):
     """
     Change background with an imported picture
     """
 
-    change_bg = alter_bg()
-    change_bg.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
-    output_img = change_bg.change_bg_img(frame, background_img, output_image_name="new_img.jpg")
+    change_bg = alter_bg(model_type = "pb")
+    model_path = "models/pixellib_models/xception_pascalvoc.pb"
+    change_bg.load_pascalvoc_model(model_path)
+    output_img = change_bg.change_frame_bg(frame, background_img_path, detect="person")
+    #change_bg.change_camera_bg(vid, background_img_path, frames_per_second = 60, show_frames=True, frame_name="frame", output_video_name="output_video.mp4", detect = "person")
 
     return output_img
 
