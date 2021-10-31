ce062e38030f5373202db2c78a077096e90f72d9
ThangDuong59 <60088921+ThangDuong59@users.noreply.github.com>
2021-10-31 19:47:27 +0700

    intialize RecordVideo object and testing record method

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from utils.record_capture import Record_Video_Capture
+from utils.record_capture import RecordVideo
 
 def main():
-    seconds = 10
-    Record_Video_Capture(seconds)
+    effects_name = "background_removal"
+    record_directory_name = "recorded_videos"
+    record_name = "recorded_video.mp4"
+    import_pic_path = None
+    import_video_path = None
+    record_time = 10
+
+    recorder = RecordVideo()
+    recorder.record_video_capture()
 
 if __name__ == '__main__':
     main()
