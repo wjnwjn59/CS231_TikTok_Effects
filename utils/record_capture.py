7d8d6670f1849b6f3e27a979b72bbd9828c8df95
ThangDuong59 <60088921+ThangDuong59@users.noreply.github.com>
2021-10-31 18:16:10 +0700

    create videos directory before save video

--- utils/record_capture.py
+++ utils/record_capture.py
@@ -1,15 +1,18 @@
 import cv2
 import time
+import os
 
 def Record_Video_Capture(time_capture):
     vid = cv2.VideoCapture(0)
-    name_video = "save_video.mp4"
-    save_vid = cv2.VideoWriter(name_video,-1,20.0,(640,480))
-
+    recorded_video_folder_name = "recorded_videos"
+    if not os.path.isdir(recorded_video_folder_name):
+        os.mkdir(recorded_video_folder_name)
+    video_name = os.path.join(recorded_video_folder_name, "save_video.mp4")
+    save_vid = cv2.VideoWriter(video_name, -1, 20.0, (640,480))
     time_cap = time_capture
     start_time = time.time()
-    while (int(time.time()-start_time)<time_capture):
-        ret,frame = vid.read()
+    while (int(time.time() - start_time) < time_capture):
+        ret, frame = vid.read()
 
         if ret:
             save_vid.write(frame)
@@ -24,7 +27,4 @@ def Record_Video_Capture(time_capture):
     save_vid.release()
     cv2.destroyAllWindows()
 
-
-
-Record_Video_Capture(10)
 
