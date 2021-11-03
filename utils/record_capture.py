e9b7c01e04d3c1c06eb50c27dc69a626f0f809a2
ThangDuong59 <60088921+ThangDuong59@users.noreply.github.com>
2021-11-03 10:02:30 +0700

    delete call_effects function and change to if else

--- utils/record_capture.py
+++ utils/record_capture.py
@@ -19,12 +19,6 @@ class RecordVideo(object):
         self.import_video_path = import_video_path
         self.effects = effects
 
-    def call_effects(self):
-        if self.effects == "background_removal":
-            return lambda frame: utils.effects.background_removal_effect(frame, self.import_pic_path)
-        else:
-            return lambda frame: utils.effects.do_nothing_effect(frame)
-
     def record_video_capture(self):
         vid = cv2.VideoCapture(0)
         if not os.path.isdir(self.record_directory_name):
@@ -33,20 +27,48 @@ class RecordVideo(object):
         save_vid = cv2.VideoWriter(video_name, -1, 20.0, (640,480))
         start_time = time.time()
         
-        effect_func = self.call_effects()
 
-        while (vid.isOpened()):
-            ret, frame = vid.read()
+        if self.effects == "background_removal":
+            while (vid.isOpened()):
+                ret, frame = vid.read()
 
-            if ret:
-                effect_frame = effect_func(frame)
-                save_vid.write(effect_frame)
+                if ret:
+                    effect_frame = utils.effects.background_removal_effect(frame, self.import_pic_path)
+                    save_vid.write(effect_frame)
 
-                cv2.imshow("frame", effect_frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    cv2.imshow("frame", effect_frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                else:
                     break
-            else:
-                break
+        elif self.effects == "zoom_in":
+            frame_count = 0
+            stop_zoom = 100
+            smooth = 5
+            while (vid.isOpened()):
+                ret, frame = vid.read()
+                if ret:
+                    effect_frame = utils.effects.zoom_in_effect(frame, frame_count, stop_zoom, smooth)
+                    frame_count += 1
+                    save_vid.write(effect_frame)
+
+                    cv2.imshow("frame", effect_frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                else:
+                    break
+        else:
+            while (vid.isOpened()):
+                ret, frame = vid.read()
+
+                if ret:
+                    save_vid.write(frame)
+
+                    cv2.imshow("frame", frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                else:
+                    break
 
         vid.release()
         save_vid.release()
