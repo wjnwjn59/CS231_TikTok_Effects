0eba18d94f43c037f2edc4fa8c7c0703d9597214
ThangDuong59 <60088921+ThangDuong59@users.noreply.github.com>
2021-11-07 17:35:10 +0700

    add comments for main.py

--- main.py
+++ main.py
@@ -55,6 +55,8 @@ def main():
     args = parse_arguments()
     # change effect name to this list to use effects:
     ## "background_removal": change background to specific image from 'import_pic_path', use GPU to speed up recording
+    ## "zoom_in"
+    ## "sepia"
     effect_name = args.effect_name
     record_directory_name = args.output_dir
     record_name = args.output_video_name
