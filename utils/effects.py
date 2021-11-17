e490390b28340781fe02e200b5caa6ab04bce6e8
ThangDuong59 <60088921+ThangDuong59@users.noreply.github.com>
2021-11-17 18:44:50 +0700

    comment background_removal_effect function and import pixellib for debug

--- utils/effects.py
+++ utils/effects.py
@@ -1,20 +1,20 @@
 import cv2
 import numpy as np 
-import pixellib
-from pixellib.tune_bg import alter_bg
+# import pixellib
+# from pixellib.tune_bg import alter_bg
 
-def background_removal_effect(frame, background_img_path):
-    """
-    Change background with an imported picture
-    """
+# def background_removal_effect(frame, background_img_path):
+#     """
+#     Change background with an imported picture
+#     """
 
-    change_bg = alter_bg(model_type = "pb")
-    model_path = "models/pixellib_models/xception_pascalvoc.pb"
-    change_bg.load_pascalvoc_model(model_path)
-    output_img = change_bg.change_frame_bg(frame, background_img_path, detect="person")
-    #change_bg.change_camera_bg(vid, background_img_path, frames_per_second = 60, show_frames=True, frame_name="frame", output_video_name="output_video.mp4", detect = "person")
+#     change_bg = alter_bg(model_type = "pb")
+#     model_path = "models/pixellib_models/xception_pascalvoc.pb"
+#     change_bg.load_pascalvoc_model(model_path)
+#     output_img = change_bg.change_frame_bg(frame, background_img_path, detect="person")
+#     #change_bg.change_camera_bg(vid, background_img_path, frames_per_second = 60, show_frames=True, frame_name="frame", output_video_name="output_video.mp4", detect = "person")
 
-    return output_img
+#     return output_img
 
 
 def zoom_in_effect(img, count_frame, stop_zoom=50, smooth=5):
@@ -62,4 +62,5 @@ def sepia_effect(img):
     img_sepia = np.array(img_sepia, dtype=np.uint8)
     
     return img_sepia
+
 
