d4eb0ddb1696c436f60e195258620413e1866713
ThangDuong59 <60088921+ThangDuong59@users.noreply.github.com>
2021-11-21 17:03:36 +0700

    add haar cascades face recognition

--- utils/record_capture.py
+++ utils/record_capture.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import sys
 import os
 import numpy as np
 import utils.effects
@@ -105,7 +106,36 @@ class RecordVideo(object):
                       break
                 else:
                    break
-                 
+        elif self.effects == "face_recognition":
+            cv2_base_dir = os.path.dirname(os.path.abspath(cv2.__file__))
+            cascPath = os.path.join(cv2_base_dir, 'data/haarcascade_frontalface_default.xml')
+            faceCascade = cv2.CascadeClassifier(cascPath)
+            while (vid.isOpened()):
+                 # Capture frame-by-frame
+                ret, frame = vid.read()
+
+                if ret:
+                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                    faces = faceCascade.detectMultiScale(
+                        gray, 
+                        scaleFactor=1.05,
+                        minNeighbors=7, 
+                        minSize=(30, 30),
+                        flags=cv2.CASCADE_SCALE_IMAGE
+                    )
+
+                    # Draw a rectangle around the faces
+                    for (x, y, w, h) in faces:
+                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    save_vid.write(frame)
+                    # Display the resulting frame
+                    cv2.imshow("frame", frame)
+
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                else:
+                    break
         else:
             while (vid.isOpened()):
                 ret, frame = vid.read()
