33b0df85a671fda7e7fab7517340f8b74b95c228
ThangDuong59 <60088921+ThangDuong59@users.noreply.github.com>
2021-11-28 13:44:57 +0700

    do nothing

--- utils/effects.py
+++ utils/effects.py
@@ -3,14 +3,15 @@ import numpy as np
 # import pixellib
 # from pixellib.tune_bg import alter_bg
 
+# change_bg = alter_bg(model_type = "pb")
+# model_path = "models/pixellib_models/xception_pascalvoc.pb"
+# change_bg.load_pascalvoc_model(model_path)
+
 # def background_removal_effect(frame, background_img_path):
 #     """
 #     Change background with an imported picture
 #     """
 
-#     change_bg = alter_bg(model_type = "pb")
-#     model_path = "models/pixellib_models/xception_pascalvoc.pb"
-#     change_bg.load_pascalvoc_model(model_path)
 #     output_img = change_bg.change_frame_bg(frame, background_img_path, detect="person")
 #     #change_bg.change_camera_bg(vid, background_img_path, frames_per_second = 60, show_frames=True, frame_name="frame", output_video_name="output_video.mp4", detect = "person")
 
