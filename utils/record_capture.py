238edf0ad49c38b94dd18f881506a85252e1944d
DNThuan <67100446+DNThuanUIT@users.noreply.github.com>
2021-12-06 00:04:46 +0700

    add effect pig_nose

--- utils/record_capture.py
+++ utils/record_capture.py
@@ -4,6 +4,8 @@ import sys
 import os
 import numpy as np
 import utils.effects
+import dlib
+from math import hypot
 
 class RecordVideo(object):
     def __init__(self, 
@@ -156,6 +158,58 @@ class RecordVideo(object):
                         break
                 else:
                     break
+
+        elif self.effects == "pig_nose":
+            nose_image = cv2.imread("import_pics/pig_nose.png")
+            _, frame = vid.read()
+            rows, cols, _ = frame.shape
+            nose_mask = np.zeros((rows, cols), np.uint8)
+
+            detector = dlib.get_frontal_face_detector()
+            predictor = dlib.shape_predictor("static/files/shape_predictor_68_face_landmarks.dat")
+
+            while (vid.isOpened()):
+                ret, frame = vid.read()
+                if ret:
+                    nose_mask.fill(0)
+                    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    faces = detector(frame)
+
+                    for face in faces:
+                        landmarks = predictor(gray_frame, face)
+
+                        top_nose = (landmarks.part(29).x, landmarks.part(29).y)
+                        center_nose = (landmarks.part(30).x, landmarks.part(30).y)
+                        left_nose = (landmarks.part(31).x, landmarks.part(31).y)
+                        right_nose = (landmarks.part(35).x, landmarks.part(35).y)
+
+                        nose_width = int(hypot(left_nose[0] - right_nose[0],
+                                        left_nose[1] - right_nose[1]) * 1.7)
+                        nose_height = int(nose_width * 0.77)
+
+                        top_left = (int(center_nose[0] - nose_width / 2),
+                                            int(center_nose[1] - nose_height / 2))
+                        bottom_right = (int(center_nose[0] + nose_width / 2),
+                                    int(center_nose[1] + nose_height / 2))
+
+                        nose_pig = cv2.resize(nose_image, (nose_width, nose_height))
+                        nose_pig_gray = cv2.cvtColor(nose_pig, cv2.COLOR_BGR2GRAY)
+                        _, nose_mask = cv2.threshold(nose_pig_gray, 25, 255, cv2.THRESH_BINARY_INV)
+
+                        nose_area = frame[top_left[1]: top_left[1] + nose_height,
+                                    top_left[0]: top_left[0] + nose_width]
+                        nose_area_no_nose = cv2.bitwise_and(nose_area, nose_area, mask=nose_mask)
+                        final_nose = cv2.add(nose_area_no_nose, nose_pig)
+
+                        frame[top_left[1]: top_left[1] + nose_height,
+                                    top_left[0]: top_left[0] + nose_width] = final_nose
+                    cv2.imshow("Frame", frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                else:
+                    break
+
+        
         else:
             while (vid.isOpened()):
                 ret, frame = vid.read()
